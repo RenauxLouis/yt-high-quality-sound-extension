a5cf9d608176856d440d12c205162fa706015bb4
RenauxLouis <renauxlouis@gmail.com>
2022-01-07 22:32:44 -0500

    fix

--- content.js
+++ content.js
@@ -1,8 +1,6 @@
 const BUCKET_NAME = "pure-asmr";
 const YOUTUBE_PREFIX = "https://www.youtube.com/watch?v=";
 
-console.log("coucou");
-
 //TODO: Move AWS init to background.js
 const accessKeyId = "AKIAUEKUXBWYSUCT7QCF";
 const secretAccessKey = "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";
@@ -54,8 +52,6 @@ async function adsPassed() {
   const video = document.querySelector("video");
 
   while (Math.floor(video.duration) != expectedDuration) {
-    console.log(Math.floor(video.duration));
-    console.log("wait cause ads");
     let promise = adsPassed2();
     let result = await promise;
     adsPassed();
@@ -95,7 +91,6 @@ async function audioLoaded(videoID) {
 async function streamMusic() {
 
   let validVideoIDS = await getDB();
-  console.log(validVideoIDS);
 
   const url = location.href;
   const videoID = url.split("&")[0].replace(YOUTUBE_PREFIX, "");
@@ -103,12 +98,10 @@ async function streamMusic() {
   if (videoInDB(url, videoID, validVideoIDS)) {
 
     let [flagAdsPassed, audioElementPromise] = await Promise.allSettled([adsPassed(), audioLoaded(videoID)]);
-    console.log(flagAdsPassed);
 
     var audioElement = audioElementPromise.value;
 
     const video = document.querySelector("video");
-    console.log(video.duration);
 
     video.pause();
 
@@ -116,44 +109,28 @@ async function streamMusic() {
     video.currentTime = 0;
 
     audioElement.addEventListener('canplaythrough', (event) => {
-      console.log('I think I can play through the entire audio without ever having to stop to buffer.');
-      //MuteUnmute(mute = true);
       video.play();
-      console.log(video.muted);
       video.muted = true;
-      console.log(video.muted);
       audioElement.play();
     });
 
-    console.log("Played")
-
-    //TODO: FIX WHEN USE OF SHORTCUTS
-    //TODO: FIX WHEN CLICK ON VIDEO TO PAUSE
-
-    // PAUSE/RESUME AUDIO FILE WHEN USER CLICKS PLAY/PAUSE ON THE VIDEO
-    playButton = document.getElementsByClassName("ytp-play-button")[0];
-    playButton.addEventListener("click", function () {
-      videoPaused = video.paused;
-      if (videoPaused) {
-        console.log("pause");
-        audioElement.pause();
-      } else {
-        console.log("resume");
+    // PLAY/PAUSE AUDIO FILE WHEN VIDEO PLAY/PAUSE
+    video.addEventListener("pause", (event) => {
+      audioElement.pause();
+    })
+    video.addEventListener("play", (event) => {
         audioElement.play();
-      };
-    });
+    })
 
     // MOVE AUDIO FILE TIME WHEN USER CLICKS IT ON THE VIDEO
     //TODO: Fix issue when click drag and cursor out of image
     progressBar = document.getElementsByClassName("ytp-progress-bar")[0];
     progressBar.addEventListener("click", function () {
-      console.log(video.currentTime);
       audioElement.currentTime = video.currentTime;
     });
 
     muteButton = document.getElementsByClassName("ytp-mute-button")[0];
     muteButton.addEventListener("click", function () {
-      console.log("mute button clicked");
       if (audioElement.muted) {
         audioElement.muted = false;
       } else {
@@ -164,9 +141,8 @@ async function streamMusic() {
 
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       if (request.message === 'TabUpdated') {
-        console.log("need to stop music and unmute if necessary");
-        audioElement.pause();
-        MuteUnmute(mute=false);
+        console.log("reload");
+        location.reload();
       }
     })
   }
